a5779e4ee010b9fb9b0e753bae5fb016708cb794
TXCthompson <91626081+TXCthompson@users.noreply.github.com>
2021-11-22 14:39:04 +0200

    Add files via upload

---
+++ таблица умножения.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	// Сизченко СOI-21-1
+	for (int t=1; t<=10; t++ )
+	{
+		for (int tt=1; tt<=10; tt++)
+        cout << t*tt << " ";
+    }
+	return 0;
+}
