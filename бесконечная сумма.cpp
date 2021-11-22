509f7797150fbe1026a48095149704de8453c74f
TXCthompson <91626081+TXCthompson@users.noreply.github.com>
2021-11-22 14:39:44 +0200

    Add files via upload

---
+++ бесконечная сумма.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+   // Сизченко СOI-21-1
+   double a=0.5, b=0.0909090909, c=0.03846153846, d=0.02127659574;
+   double s = a+b+c+d;
+   printf("%10.3f", s);
+   return 0;
+}
